96ef0ddf1f34fdad34b4dc41b69df1fedf126c23
nk-on <2nkoz@proton.me>
2025-07-29 22:47:47 +0400

    Feat:sending simple welcome message to the user

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-
+    const pathName = req.url;
+    if(pathName === '/'){
+        res.end('<h1>Welcome to my page</h1>');
+    }
 });
 server.listen(8000,()=>{
     console.log('Server is working')
