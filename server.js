3995409e9773e70f24967c82e035606806d71ad1
nk-on <2nkoz@proton.me>
2025-07-30 21:50:35 +0400

    Feat:responding user with API if they specify it inside route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs')
 const server = http.createServer((req,res)=>{
     const pathName = req.url;
     if(pathName === '/'){
@@ -9,6 +10,9 @@ const server = http.createServer((req,res)=>{
         res.statusCode = 200;
         res.setHeader('Content-Type','text/html');
         res.end('<h1>Lorem ipsum</h1>');
+    }else if(pathName === '/api'){
+        const data = fs.readFileSync('data.json','utf-8');
+        res.end(`${data}`);
     }
 });
 server.listen(8000,()=>{
